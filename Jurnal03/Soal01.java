0d5e33c5e89647715dc02355fa871835cd3c14ae
novistaameliaputri <novistaamelia@gmail.com>
2020-09-24 12:11:17 +0700

    Jurnal03

---
+++ Jurnal03/Soal01.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Soal01 {
+public static void main(String[] args){
+Scanner amd = new Scanner (System.in);
+int bilangan;
+System.out.println("Masukan bilangan bulat = ");
+bilangan = amd.nextInt();
+
+bilangan = bilangan * bilangan;
+System.out.println("Hasilnya adalah "+ bilangan);
+
+}
+}
